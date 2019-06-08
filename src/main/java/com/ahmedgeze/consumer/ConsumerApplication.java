4fd51a47edf4bfc5bf94be6ddf7c81f49c8f9457
ahmetserdargeze <gezeahmetserdar@gmail.com>
2019-06-08 03:13:28 +0300

    run on compose

--- src/main/java/com/ahmedgeze/consumer/ConsumerApplication.java
+++ src/main/java/com/ahmedgeze/consumer/ConsumerApplication.java
@@ -2,9 +2,14 @@ package com.ahmedgeze.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
+@EnableKafka
 public class ConsumerApplication {
 
     public static void main(String[] args) {
@@ -13,6 +18,7 @@ public class ConsumerApplication {
 
     @KafkaListener(topics = "Topic1",groupId = "group-id")
     public void listen(String message) {
+
         System.out.println("Received Messasge in group - group-id: " + message);
     }
 
--- src/main/java/com/ahmedgeze/consumer/KafkaConfiguration.java
+++ src/main/java/com/ahmedgeze/consumer/KafkaConfiguration.java
@@ -1,10 +1,14 @@
 package com.ahmedgeze.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,11 +16,9 @@ public class KafkaConfiguration {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.17.0.1:9092");
-        props.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, "group-id");
+//        props.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
         props.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
-        props.put(org.apache.kafka.clients.consumer.ConsumerConfig.RECONNECT_BACKOFF_MS_CONFIG, "10000");
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
