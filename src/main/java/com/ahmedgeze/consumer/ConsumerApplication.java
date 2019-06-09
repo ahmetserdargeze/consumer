c996d593f3923bc0aabee76129622afc9562ef05
ahmet serdar <ahmedgeze@gmai.com>
2019-06-10 01:55:25 +0300

    delete un used code

--- src/main/java/com/ahmedgeze/consumer/ConsumerApplication.java
+++ src/main/java/com/ahmedgeze/consumer/ConsumerApplication.java
@@ -1,25 +1,48 @@
 package com.ahmedgeze.consumer;
 
+import com.ahmedgeze.consumer.entity.LogEntity;
+import com.ahmedgeze.consumer.repository.LogEntityRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.io.IOException;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableKafka
-public class ConsumerApplication {
+public class ConsumerApplication implements WebMvcConfigurer {
+
+    @Autowired
+    LogEntityRepository logEntityRepository;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+    }
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
-    @KafkaListener(topics = "Topic1",groupId = "group-id")
-    public void listen(String message) {
-
-        System.out.println("Received Messasge in group - group-id: " + message);
+    @KafkaListener(topics = "Topic1", groupId = "group-id")
+    public void listen(String message) throws IOException {
+        LogEntity logEntity = objectMapper.readValue(message, LogEntity.class);
+        logEntityRepository.save(logEntity);
+        template.convertAndSend("/queue/now", message);
     }
 
 }
---
+++ src/main/java/com/ahmedgeze/consumer/entity/LogEntity.java
@@ -0,0 +1,83 @@
+package com.ahmedgeze.consumer.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "Logs")
+public class LogEntity  implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "log_type")
+    private String logLevel;
+
+    @Column(name = "city")
+    private String city;
+
+
+    @Column(name = "log_detail")
+    private String logDetail;
+
+    @Column(name = "create_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private Date date;
+
+    public LogEntity() {
+    }
+
+    public LogEntity(String logLevel, String city, String logDetail, Date date) {
+        this.logLevel = logLevel;
+        this.city = city;
+        this.logDetail = logDetail;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(String logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getLogDetail() {
+        return logDetail;
+    }
+
+    public void setLogDetail(String logDetail) {
+        this.logDetail = logDetail;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
